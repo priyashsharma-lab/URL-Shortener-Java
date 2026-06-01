f2cb91f6dbb2b07f53914076c4305ce8d3acb755
Priyash Sharma <priyasharma496@gmail.com>
2026-06-01 19:39:41 +0530

    Release v2.0: added visit tracking, duplicate URL detection, and improved encoding

--- src/main/java/com/shorturl/service/EncodeUrl.java
+++ src/main/java/com/shorturl/service/EncodeUrl.java
@@ -1,6 +1,7 @@
 package com.shorturl.service;
 
 import com.shorturl.dao.DatabaseManager;
+import com.shorturl.util.DBUtil;
 import com.shorturl.util.UrlInfo;
 
 public class EncodeUrl 
@@ -14,6 +15,11 @@ public class EncodeUrl
     }
     public String generateEncodedUrl()
     {
+        if (DBUtil.isOriginalUrlExists(originalUrl))
+        {
+            encodedUrl=DBUtil.getEncodedUrl(originalUrl);
+            return encodedUrl;
+        }
         UrlInfo urlinfo=new UrlInfo(originalUrl);
         if (!urlinfo.isValidUrl())
         {
@@ -29,7 +35,16 @@ public class EncodeUrl
         for (int i=0;i<3;i++)
         {
             int c=(int)domainName.charAt(i);
-            c+=3;
+            switch(c)
+            {
+                case 120: c=97; break;
+                case 121: c=98; break;
+                case 122: c=99; break;
+                case 88: c=65; break;
+                case 89: c=66; break;
+                case 90: c=67; break;
+                default: c+=3; break;
+            }
             encodedUrl+=(char)c;
         }
         encodedUrl+="-";
--- src/main/java/com/shorturl/util/DBUtil.java
+++ src/main/java/com/shorturl/util/DBUtil.java
@@ -2,6 +2,8 @@ package com.shorturl.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBUtil
@@ -12,5 +14,47 @@ public class DBUtil
     public static Connection getConnection() throws SQLException
     {
         return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+    }
+
+    public static boolean isOriginalUrlExists(String originalUrl)
+    {
+        try
+        {
+            Connection con=DBUtil.getConnection();
+            PreparedStatement pst=con.prepareStatement("Select * from urlcodes where originalUrl=?");
+            pst.setString(1, originalUrl);
+            ResultSet rs=pst.executeQuery();
+            boolean b=rs.next();
+            pst.close();
+            con.close();
+            return b;
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Database Error from isOriginalUrlExixsts in DBUtil--> "+e.getMessage());
+            return false;
+        }
+    }
+
+    public static String getEncodedUrl(String originalUrl)
+    {
+        String encodedUrl="";
+        try
+        {
+            Connection con=DBUtil.getConnection();
+            PreparedStatement pst=con.prepareStatement("Select encodedUrl from urlcodes where originalUrl=?");
+            pst.setString(1, originalUrl);
+            ResultSet rs=pst.executeQuery();
+            if (rs.next())
+            {
+                encodedUrl=rs.getString("encodedUrl");
+            }
+            return  encodedUrl;
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Database Error from getEncodedUrl in DBUtil--> "+e.getMessage());
+            return "Error";
+        }
     }
 }
