d7cb2600f12f84c38a28b6780396c19e44bf508f
Priyash Sharma <priyasharma496@gmail.com>
2026-06-02 02:44:52 +0530

    Added Copy to clipboard Feature

--- src/main/java/com/shorturl/ui/Gui.java
+++ src/main/java/com/shorturl/ui/Gui.java
@@ -1,10 +1,13 @@
 package com.shorturl.ui;
 
 import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -24,8 +27,8 @@ public class Gui
         mainFrame.setLayout(new GridLayout(2,1));
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        JPanel shortPanel=new JPanel(new GridLayout(2,2));
-        JPanel decodePanel=new JPanel(new GridLayout(2,2));
+        JPanel shortPanel=new JPanel(new GridLayout(2,3));
+        JPanel decodePanel=new JPanel(new GridLayout(2,3));
 
         JLabel shortLabel=new JLabel("Enter URL to shorten: ");
         JLabel decodeLabel=new JLabel("Enter shortURL to decode: ");
@@ -64,6 +67,26 @@ public class Gui
             String originalUrl=decUrl.getOriginalUrl();
             decodeOutputLabel.setText(originalUrl);
         });
+
+        //Copy Feature code
+        JButton shortCopyButton=new JButton("Copy");
+        JButton decodeCopyButton=new JButton("Copy");
+        shortPanel.add(shortCopyButton);
+        decodePanel.add(decodeCopyButton);
+
+        shortCopyButton.addActionListener(e->{
+            String encUrl=shortOutputLabel.getText();
+            StringSelection selection=new StringSelection(encUrl);
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, null);
+            JOptionPane.showMessageDialog(null,"URL Copied Successfully");
+        });
+        decodeCopyButton.addActionListener(e->{
+            String oriUrl=decodeOutputLabel.getText();
+            StringSelection selection=new StringSelection(oriUrl);
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, null);
+            JOptionPane.showMessageDialog(null,"URL Copied Successfully");
+        });
+
         mainFrame.setVisible(true);
     }   
 }
